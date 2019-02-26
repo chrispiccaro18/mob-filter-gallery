815741e8790f88a1df331f508a81fde6e0ec6f13
Tommy Tran <tranttommy95@gmail.com>
2019-02-26 13:55:47 -0800

    first commit, first test passed (cheating)

---
+++ test/image-component.test.js
@@ -0,0 +1,32 @@
+const test = QUnit.test;
+
+QUnit.module('image component');
+
+function createImageTemplate() {
+    return /*html*/`
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+            <h2>Horns: 1</h2>
+        </li>
+    `;
+}
+
+
+test('image template', function(assert) {
+    //arrange
+    const expected = /*html*/`
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+            <h2>Horns: 1</h2>
+        </li>
+    `;
+
+    //act
+    const result = createImageTemplate();
+
+
+    //assert    
+    assert.equal(result, expected);
+});
